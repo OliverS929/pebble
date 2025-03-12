8e372ffdb8b448f06316b5a784a911cdf5b0aa85
OliverS929 <182192954+OliverS929@users.noreply.github.com>
2025-03-12 23:08:29 +0800

    fix log print.

--- internal/cache/clockpro.go
+++ internal/cache/clockpro.go
@@ -474,7 +474,7 @@ func (c *shard) metaAdd(key key, e *entry) bool {
 func (c *shard) metaAddDebug(key key, e *entry) bool {
 	c.evict()
 	if e.size > c.targetSize() {
-		c.logger.Infof("metaAddDebug: entry cannot fit into cache with key %v and size %d", key, e.size)
+		c.logger.Info("metaAddDebug: entry cannot fit into cache with key %v and size %d", key, e.size)
 		// The entry is larger than the target cache size.
 		return false
 	}
